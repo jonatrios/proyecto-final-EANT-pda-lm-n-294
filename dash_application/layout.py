df152c9347bddfb62a64ff97c608ae31f71c3342
Fernando Gonzalez <fernandogonzalez@MacBook-Air-de-Fernando.local>
2021-10-21 19:57:48 -0300

    Modificando texto en side bar

--- dash_application/layout.py
+++ dash_application/layout.py
@@ -43,7 +43,8 @@ sidebar = html.Div(
         html.H2([html.A(html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode())), href="/", title='Volver al inicio')], className="rounded mx-auto d-block"),
         html.Hr(),
         html.P(
-            "Cada boton representa el analisis realizado de un dataset", className="lead"
+            #"Cada boton representa el analisis realizado de un dataset", className="lead"
+            "Python Data Analytics - pda-lm-n-294 - Grupo 1", className="lead"
         ),
         dbc.Nav(
             [

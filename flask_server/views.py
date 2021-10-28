aad03947902f3982c85a1e1770190ace609899e6
Fernando Gonzalez <fernandogonzalez@MacBook-Air-de-Fernando.local>
2021-10-28 10:26:18 -0300

    Redireccionando el home a dash

--- flask_server/views.py
+++ flask_server/views.py
@@ -1,14 +1,12 @@
 from flask import render_template
 from flask.views import MethodView
 from flask_server import appbuilder
-from flask_server import redirect
 
 
 class HelloMethodView(MethodView):
     
     def get(self):
-        #return render_template('index.html', appbuilder=appbuilder)
-        return redirect("https://proyecto-final-eant-2021.herokuapp.com/dash/", code=302)
+        return render_template('index.html', appbuilder=appbuilder)
 
 
 
